7aff70a8e4af7a1006668e2325018daf712f2008
Frank Denis <github@pureftpd.org>
2020-01-08 21:58:37 -0700

    Namespace and bump

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PADMÉ padding scheme for Rust
 
+[![docs.rs](https://docs.rs/padme-padding/badge.svg)](https://docs.rs/padme-padding)
+
 PADMÉ limits information leakage about the length of the plain-text for wide range of encrypted data sizes.
 
 See the [PURBs](https://bford.info/pub/sec/purb.pdf) paper for details.
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 #![no_std]
 
-/// Return the padded length
-pub fn padded_len(len: usize) -> usize {
-    let e = 63usize.saturating_sub(len.leading_zeros() as usize);
-    let s = 64 - e.leading_zeros() as usize;
-    let z = e - s;
-    let mask = (1usize << z) - 1;
-    (len + mask) & !mask
-}
+pub struct Padme;
 
-/// Return the padding length
-#[inline]
-pub fn padding_len(len: usize) -> usize {
-    padded_len(len) - len
+impl Padme {
+    /// Return the padded length
+    pub fn padded_len(len: usize) -> usize {
+        let e = 63usize.saturating_sub(len.leading_zeros() as usize);
+        let s = 64 - e.leading_zeros() as usize;
+        let z = e - s;
+        let mask = (1usize << z) - 1;
+        (len + mask) & !mask
+    }
+
+    /// Return the padding length
+    #[inline]
+    pub fn padding_len(len: usize) -> usize {
+        Self::padded_len(len) - len
+    }
 }
